ed78d7ca00f421e61020a6a93e5799210d391297
Matthew <sch93nz@hotmail.com>
2017-07-21 17:31:05 +1200

    i made some errors and i ahve fixed them also changes the style of table i am making

--- FirstTest.php
+++ FirstTest.php
@@ -15,14 +15,14 @@ $sql = "SELECT * FROM pet";
 $result = $conn->query($sql);
 
 if( $result->num_rows > 0) {
-    echo '<table style="width:100%">';
+    echo '<table style="width:20%">';
     
 while($row = $result->fetch_assoc()){
     echo '<tr>';
-    echo '<th>'.$row[name].'</th>';
-    echo '<th>'.$row[owner].'</th>';
-    echo '<th>'.$row[species].'</th>';
-    echo '<th>'.$row[sex].'</th>';
+    echo '<th>' . $row[name] . '</th>';
+    echo '<th>' . $row[owner] . '</th>';
+    echo '<th>' . $row[species] . '</th>';
+    echo '<th>' . $row[sex] . '</th>';
 }
 } else {
     echo "0 results";
