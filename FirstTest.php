4a047f7d5cf519c081900e4bae1f1a33ced9a498
Matthew <sch93nz@hotmail.com>
2017-07-21 17:55:37 +1200

    now trying to build a page around the query i just created

--- FirstTest.php
+++ FirstTest.php
@@ -2,41 +2,40 @@
 <html>
 <meta charset="UTF-8">
 
-<head>
+<header>
     <title>Here is a Quick Test of PHP with Mysql</title>
     </br>
-</head>
+</header>
 
 <body>
-
     <div>
         <?php
-$servername = "localhost";
-$username = "wordpressuser";
-$password = "Password?00";
-$dbname = "wordpress";
+            $servername = "localhost";
+            $username = "wordpressuser";
+            $password = "Password?00";
+            $dbname = "wordpress";
 
-$conn = new mysqli($servername,$username,$password,$dbname);
+            $conn = new mysqli($servername,$username,$password,$dbname);
 
-if ($conn->connect_error){
-    die("Connection failed: ". $conn->connect_error);
-}
+            if ($conn->connect_error){
+                die("Connection failed: ". $conn->connect_error);
+            }
 
-$sql = "SELECT * FROM pet";
+            $sql = "SELECT * FROM pet";
 
-$result = $conn->query($sql);
+            $result = $conn->query($sql);
 
-if( $result->num_rows > 0) {
-    echo '<table style="width:20%">' . '<tr>' . '<th>name</th>' . '<th>owner</th>' . '<th>species</th>' . '<th>sex</th>' . '</tr>';
-while($row = $result->fetch_assoc()){
-    echo '<tr>' . '<th>' . $row[name] . '</th>' . '<th>' . $row[owner] . '</th>' . '<th>' . $row[species] . '</th>' . '<th>' . $row[sex] . '</th>' . '</tr>';
-}
-} else {
-    echo "0 results";
-}
+            if( $result->num_rows > 0) {
+                echo '<table style="width:20%">' . '<tr>' . '<th>name</th>' . '<th>owner</th>' . '<th>species</th>' . '<th>sex</th>' . '</tr>';
+                while($row = $result->fetch_assoc()){
+                    echo '<tr>' . '<th>' . $row[name] . '</th>' . '<th>' . $row[owner] . '</th>' . '<th>' . $row[species] . '</th>' . '<th>' . $row[sex] . '</th>' . '</tr>';
+                }
+            } else {
+                echo "0 results";
+            }
 
-$conn->close();
-?>
+            $conn->close();
+            ?>
     </div>
 </body>
 <footer>
