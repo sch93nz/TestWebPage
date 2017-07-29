d22426ef801d7604901fb9137045e37b61dcc131
Matthew <sch93nz@hotmail.com>
2017-07-29 16:34:29 +1200

    trying to fix a bug

--- Solitaire/index.php
+++ Solitaire/index.php
@@ -48,9 +48,10 @@ if (!isset($_SESSION['Deck'])){
         function onFunc(){
             echo $_SESSION["Index"];
             $value = $_SESSION["Index"];
-            $card = $_SESSION['Deck'][ $value ];
-            echo $value;
-            
+            $card = $_SESSION['Deck'];
+
+            echo $card[$value];
+
             $data =  $card -> getCard();
             echo '<img src="' . $data . '">';
         }
